591e85334af68d17c0c9fe4afeb41c683e8a0000
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2021-05-26 16:13:55 +0200

    init

---
+++ business-tracker-api/src/test/java/de/telran/businesstracker/BusinessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.businesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
