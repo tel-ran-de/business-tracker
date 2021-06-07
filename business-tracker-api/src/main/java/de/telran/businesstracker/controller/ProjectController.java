1992d0d2e1ef98b391062c5b31814f123e57671c
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2021-06-07 12:55:27 +0200

    add initial settings for cloak resource service

--- business-tracker-api/src/main/java/de/telran/businesstracker/controller/ProjectController.java
+++ business-tracker-api/src/main/java/de/telran/businesstracker/controller/ProjectController.java
@@ -6,6 +6,7 @@ import de.telran.businesstracker.mapper.ProjectMapper;
 import de.telran.businesstracker.service.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,6 +52,7 @@ public class ProjectController {
     }
 
     @GetMapping("")
+    @PreAuthorize("hasRole('user')")
     public List<ProjectDto> getAllProjects() {
         return projectService.getAll()
                 .stream()
---
+++ business-tracker-api/src/main/java/de/telran/businesstracker/security/WebSecurity.java
@@ -0,0 +1,71 @@
+package de.telran.businesstracker.security;
+
+import com.nimbusds.jose.shaded.json.JSONArray;
+import com.nimbusds.jose.shaded.json.JSONObject;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/api/**")
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()));
+    }
+
+    @Bean
+    public Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesAndRolesConverter());
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesAndRolesConverter() {
+        JwtGrantedAuthoritiesConverter delegate = new JwtGrantedAuthoritiesConverter();
+        //don't remake to lambda here!
+        return new Converter<Jwt, Collection<GrantedAuthority>>() {
+            @Override
+            public Collection<GrantedAuthority> convert(Jwt jwt) {
+                Collection<GrantedAuthority> grantedAuthorities = delegate.convert(jwt);
+
+                if (jwt.getClaim("realm_access") == null) {
+                    return grantedAuthorities;
+                }
+                JSONObject realmAccess = jwt.getClaim("realm_access");
+                if (realmAccess.get("roles") == null) {
+                    return grantedAuthorities;
+                }
+                JSONArray roles = (JSONArray) realmAccess.get("roles");
+
+                final List<SimpleGrantedAuthority> keycloakAuthorities = roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role)).collect(Collectors.toList());
+                grantedAuthorities.addAll(keycloakAuthorities);
+
+                return grantedAuthorities;
+            }
+        };
+    }
+
+}
+
